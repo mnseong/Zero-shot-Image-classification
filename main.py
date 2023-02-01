4367b3ab2adac5072009c69947889faccde78c31
mnseong <mskweon@icloud.com>
2023-02-01 16:16:18 +0900

    test run main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from ZeroShotClassification import ZeroShotImageClassification
+
+zsic = ZeroShotImageClassification()
+
+
+#Predictions
+preds = zsic(image="http://images.cocodataset.org/val2017/000000039769.jpg",
+            candidate_labels=["birds", "lions", "cats","dogs"],
+            )
+print(preds)
